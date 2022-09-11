4a2f05adedda6f622418f765c640d00c2d951009
Pikachu <git@simonhaefliger.ch>
2022-09-11 21:19:58 +0200

    Filament configured

---
+++ app/Providers/FilamentServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use Filament\Facades\Filament;
+use Filament\Navigation\UserMenuItem;
+use Illuminate\Support\ServiceProvider;
+
+class FilamentServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Filament::serving(function () {
+            Filament::registerUserMenuItems([
+                'logout' => UserMenuItem::make()->url(route('logout')),
+            ]);
+        });
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return redirect()->route('filament.pages.dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('dashboard/login', function () {
+    return redirect()->route('login');
+});
 
 require __DIR__.'/auth.php';
